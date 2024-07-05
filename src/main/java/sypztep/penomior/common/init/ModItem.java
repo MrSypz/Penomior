5db95b6ec59d7024b715344c73889a858a1c47e1
chai <chainot@hotmail.co.th>
2024-07-06 05:28:59 +0700

    New Item for refine talbe

--- src/main/java/sypztep/penomior/common/init/ModItem.java
+++ src/main/java/sypztep/penomior/common/init/ModItem.java
@@ -8,12 +8,12 @@ import sypztep.penomior.Penomior;
 
 public class ModItem {
     public static Item REFINE_TABLE;
-//    public static Item COPPERAL_WEAPON;
-//    public static Item COPPERAL_ARMOR;
+    public static Item REFINE_WEAPON_STONE;
+    public static Item REFINE_ARMOR_STONE;
     public static void init() {
         REFINE_TABLE = registeritem("refine_table",new BlockItem(ModBlockItem.REFINE_TABLE, new Item.Settings()));
-//        COPPERAL_WEAPON = registeritem("copperal_weapon", new Item(new Item.Settings().maxCount(99)));
-//        COPPERAL_ARMOR = registeritem("copperal_armor", new Item(new Item.Settings().maxCount(99)));
+        REFINE_WEAPON_STONE = registeritem("refine_weapon_stone", new Item(new Item.Settings().maxCount(99)));
+        REFINE_ARMOR_STONE = registeritem("refine_armor_stone", new Item(new Item.Settings().maxCount(99)));
     }
     public static <T extends Item> T registeritem(String name, T item) {
         Registry.register(Registries.ITEM, Penomior.id(name), item);
