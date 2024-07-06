43748d6e6c57a2164f1be1db8e8b40d5f4208fd3
chai <chainot@hotmail.co.th>
2024-07-06 12:25:29 +0700

    add max durability

--- src/main/java/sypztep/penomior/common/data/PenomiorItemData.java
+++ src/main/java/sypztep/penomior/common/data/PenomiorItemData.java
@@ -9,10 +9,14 @@ public record PenomiorItemData(
         int startAccuracy,
         int endAccuracy,
         int startEvasion,
-        int endEvasion
+        int endEvasion,
+        int maxDurability
 ) {
     public static PenomiorItemData getPenomiroItemData(ItemStack stack) {
         String itemID = Registries.ITEM.getId(stack.getItem()).toString();
         return PenomiorItemDataSerializer.getConfigCache().get(itemID);
+    }
+    public static String getItemId(ItemStack stack) {
+        return Registries.ITEM.getId(stack.getItem()).toString();
     }
 }
