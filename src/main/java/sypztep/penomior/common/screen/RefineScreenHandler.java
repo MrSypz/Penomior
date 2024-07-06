3b752db214d26a09849be516b6883835eeadd471
chai <chainot@hotmail.co.th>
2024-07-06 12:25:48 +0700

    QuickMove

--- src/main/java/sypztep/penomior/common/screen/RefineScreenHandler.java
+++ src/main/java/sypztep/penomior/common/screen/RefineScreenHandler.java
@@ -5,7 +5,6 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.*;
-import net.minecraft.registry.Registries;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.ScreenHandlerContext;
 import net.minecraft.screen.slot.Slot;
@@ -19,6 +18,7 @@ import sypztep.penomior.common.init.ModDataComponents;
 import sypztep.penomior.common.init.ModItem;
 import sypztep.penomior.common.init.ModScreenHandler;
 import sypztep.penomior.common.util.RefineUtil;
+
 //TODO : make a failstacks system and pri duo tet enchant
 public class RefineScreenHandler extends ScreenHandler {
     private final Inventory inventory = new SimpleInventory(3) {
@@ -78,27 +78,29 @@ public class RefineScreenHandler extends ScreenHandler {
     private void doRefineTask() {
         ItemStack slotOutput = this.getSlot(1).getStack();
 
-        if (RefineUtil.getRefineLvl(slotOutput) < 20) {
+        if (RefineUtil.getRefineLvl(slotOutput) < 20)
             this.canRefine = false;
-        }
         if (this.getSlot(0).hasStack() && this.getSlot(1).hasStack()) {
             ItemStack material = this.getSlot(0).getStack();
             if (matchesItemData(slotOutput)) {
                 boolean isArmor = slotOutput.getItem() instanceof ArmorItem;
                 boolean isRefined = slotOutput.get(ModDataComponents.PENOMIOR) != null;
 
-                if (isRefined && !isArmor) {
+                if (!isRefined && !isArmor)  //handle a null data
                     this.canRefine = material.isOf(ModItem.REFINE_WEAPON_STONE);
-                } else if (isRefined) {
+                 else if (!isRefined)
                     this.canRefine = material.isOf(ModItem.REFINE_ARMOR_STONE);
-                }
+
+                if (isRefined && !isArmor)
+                    this.canRefine = material.isOf(ModItem.REFINE_WEAPON_STONE);
+                 else if (isRefined)
+                    this.canRefine = material.isOf(ModItem.REFINE_ARMOR_STONE);
+
             }
-        } else {
+        } else
             this.canRefine = false;
-        }
         RefinePayloadS2C.send((ServerPlayerEntity) player, !this.canRefine);
     }
-
 
     @Override
     public void onClosed(PlayerEntity player) {
@@ -111,66 +113,8 @@ public class RefineScreenHandler extends ScreenHandler {
         return this.context.get((world, pos) -> player.squaredDistanceTo((double) pos.getX() + 0.5, (double) pos.getY() + 0.5, (double) pos.getZ() + 0.5) <= 64.0, true);
     }
 
-    @Override
-    public ItemStack quickMove(PlayerEntity player, int index) {
-        ItemStack stack = ItemStack.EMPTY;
-        Slot slot = this.slots.get(index);
-        //TODO : make it quick moveble
-//        if (slot != null && slot.hasStack()) {
-//            ItemStack slotStack = slot.getStack();
-//            stack = slotStack.copy();
-//
-//         If the slot clicked is one of the container's slots
-//            if (index < 3) { // 0, 1, 2 are container slots
-//                if (!insertItem(slotStack, 3, 39, true)) { // Player inventory slots: 3 to 38 (hotbar included)
-//                    return ItemStack.EMPTY;
-//                }
-//                slot.onQuickTransfer(slotStack, stack);
-//            } else {
-//                // If the slot clicked is one of the player inventory slots
-//                if (isGrinderMaterial(slotStack)) {
-//                    if (!insertItem(slotStack, 0, 1, false)) {
-//                        return ItemStack.EMPTY;
-//                    }
-//                } else if (isGrindableItem(slotStack)) {
-//                    if (!insertItem(slotStack, 1, 2, false)) {
-//                        return ItemStack.EMPTY;
-//                    }
-//                } else if (slotStack.isOf(Items.COPPER_INGOT)) {
-//                    if (!insertItem(slotStack, 2, 3, false)) {
-//                        return ItemStack.EMPTY;
-//                    }
-//                } else {
-//                    if (index >= 3 && index < 30) { // Player main inventory (excluding hotbar)
-//                        if (!insertItem(slotStack, 30, 39, false)) { // Try hotbar
-//                            return ItemStack.EMPTY;
-//                        }
-//                    } else if (index >= 30 && index < 39) { // Hotbar
-//                        if (!insertItem(slotStack, 3, 30, false)) { // Try main inventory
-//                            return ItemStack.EMPTY;
-//                        }
-//                    } else {
-//                        return ItemStack.EMPTY;
-//                    }
-//                }
-//            }
-//
-//            if (slotStack.isEmpty()) {
-//                slot.setStack(ItemStack.EMPTY);
-//            } else {
-//                slot.markDirty();
-//            }
-//
-//            if (slotStack.getCount() == stack.getCount()) {
-//                return ItemStack.EMPTY;
-//            }
-//            slot.onTakeItem(player, slotStack);
-//        }
-        return stack;
-    }
-
     private boolean matchesItemData(ItemStack stack) {
-        String itemID = Registries.ITEM.getId(stack.getItem()).toString();
+        String itemID = PenomiorItemData.getItemId(stack);
         PenomiorItemData itemData = PenomiorItemDataSerializer.getConfigCache().get(itemID);
         return itemData != null && itemID.equals(itemData.itemID());
     }
@@ -185,27 +129,39 @@ public class RefineScreenHandler extends ScreenHandler {
             boolean isArmor = slotOutput.getItem() instanceof ArmorItem;
             boolean isRefined = slotOutput.get(ModDataComponents.PENOMIOR) != null;
             PenomiorItemData itemData = PenomiorItemData.getPenomiroItemData(slotOutput);
+            int currentRefineLvl = RefineUtil.getRefineLvl(slotOutput);
             int maxLvl = itemData.maxLvl();
             int startAccuracy = itemData.startAccuracy();
             int endAccuracy = itemData.endAccuracy();
             int startEvasion = itemData.startEvasion();
             int endEvasion = itemData.endEvasion();
+            //-------------Handle null item---------------//
+            if (!isRefined && !isArmor) {
+                RefineUtil.setRefineLvl(slotOutput, 0);
+                RefineUtil.setRefineLvl(slotOutput, currentRefineLvl + 1);
 
+                RefineUtil.setAccuracy(slotOutput, RefineUtil.getRefineLvl(slotOutput), maxLvl, startAccuracy, endAccuracy);
+            } else if (!isRefined) {
+                RefineUtil.setRefineLvl(slotOutput, 0);
+                RefineUtil.setRefineLvl(slotOutput, currentRefineLvl + 1);
 
+                RefineUtil.setEvasion(slotOutput, RefineUtil.getRefineLvl(slotOutput), maxLvl, startEvasion, endEvasion);
+                RefineUtil.setAccuracy(slotOutput, RefineUtil.getRefineLvl(slotOutput), maxLvl, startAccuracy, endAccuracy);
+            }
+
+            //-------------Handle not null item---------------//
             if (isRefined && !isArmor) {
-                int currentRefineLvl = RefineUtil.getRefineLvl(slotOutput);
                 RefineUtil.setRefineLvl(slotOutput, currentRefineLvl + 1);
 
                 RefineUtil.setAccuracy(slotOutput, RefineUtil.getRefineLvl(slotOutput), maxLvl, startAccuracy, endAccuracy);
             } else if (isRefined) {
-                int currentRefineLvl = RefineUtil.getRefineLvl(slotOutput);
                 RefineUtil.setRefineLvl(slotOutput, currentRefineLvl + 1);
 
                 RefineUtil.setEvasion(slotOutput, RefineUtil.getRefineLvl(slotOutput), maxLvl, startEvasion, endEvasion);
                 RefineUtil.setAccuracy(slotOutput, RefineUtil.getRefineLvl(slotOutput), maxLvl, startAccuracy, endAccuracy);
             }
             this.decrementStack(0);
-            this.context.run((world, pos) -> world.syncWorldEvent(WorldEvents.SMITHING_TABLE_USED, pos, 0));
+            this.context.run((world, pos) -> world.syncWorldEvent(WorldEvents.GRINDSTONE_USED, pos, 0));
         }
     }
 
@@ -217,6 +173,61 @@ public class RefineScreenHandler extends ScreenHandler {
         ItemStack itemStack = this.inventory.getStack(slot);
         itemStack.decrement(1);
         this.inventory.setStack(slot, itemStack);
+    }
+
+    @Override
+    public ItemStack quickMove(PlayerEntity player, int index) {
+        ItemStack stack = ItemStack.EMPTY;
+        Slot slot = this.slots.get(index);
+        if (slot.hasStack()) {
+            ItemStack slotStack = slot.getStack();
+            stack = slotStack.copy();
+
+            if (index < 3) { // 0, 1, 2 are container slots
+                if (!insertItem(slotStack, 3, 39, true)) {
+                    return ItemStack.EMPTY;
+                }
+                slot.onQuickTransfer(slotStack, stack);
+            } else {
+                if (isRefineMaterial(slotStack)) {
+                    if (!insertItem(slotStack, 0, 1, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else if (matchesItemData(slotStack)) {
+                    if (!insertItem(slotStack, 1, 2, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else if (slotStack.isOf(Items.COPPER_INGOT)) {
+                    if (!insertItem(slotStack, 2, 3, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else {
+                    if (index < 30) { // Player main inventory (excluding hotbar)
+                        if (!insertItem(slotStack, 30, 39, false)) { // Try hotbar
+                            return ItemStack.EMPTY;
+                        }
+                    } else if (index < 39) { // Hotbar
+                        if (!insertItem(slotStack, 3, 30, false)) { // Try main inventory
+                            return ItemStack.EMPTY;
+                        }
+                    } else {
+                        return ItemStack.EMPTY;
+                    }
+                }
+            }
+
+            if (slotStack.isEmpty()) {
+                slot.setStack(ItemStack.EMPTY);
+            } else {
+                slot.markDirty();
+            }
+
+            if (slotStack.getCount() == stack.getCount()) {
+                return ItemStack.EMPTY;
+            }
+            slot.onTakeItem(player, slotStack);
+        }
+        return stack;
     }
 
     @Override
