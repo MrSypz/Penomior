9fa3c931f474e0c3ada76c4fada1338acbe50b0b
chai <chainot@hotmail.co.th>
2024-07-07 04:23:49 +0700

    more scope and visual

--- src/main/java/sypztep/penomior/common/util/RefineUtil.java
+++ src/main/java/sypztep/penomior/common/util/RefineUtil.java
@@ -1,23 +1,32 @@
 package sypztep.penomior.common.util;
 
 import net.minecraft.component.type.NbtComponent;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import sypztep.penomior.common.data.PenomiorData;
 import sypztep.penomior.common.data.PenomiorItemData;
 import sypztep.penomior.common.data.PenomiorItemDataSerializer;
 import sypztep.penomior.common.init.ModDataComponents;
+import sypztep.penomior.common.init.ModEntityComponents;
 import sypztep.tyrannus.common.util.ItemStackHelper;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class RefineUtil {
     public static Map<Integer, String> romanRefineMap = new HashMap<>();
     private static final double[] normalSuccessRates = {
             100.0, 90.0, 80.0, 70.0, 60.0, 50.0, 40.0, 30.0, 20.0, 10.0,
-            9.0, 8.0, 7.0, 6.0, 5.0
+            9.0, 8.0, 7.0, 6.0, 5.0, // Levels 1-15
+            25.0, // PRI (16)
+            17.5, // DUO (17)
+            12.5, // TRI (18)
+            7.5,  // TET (19)
+            2.5   // PEN (20)
     };
-
+    private static final double SUCCESS_RATE_CAP = 90.0; // Cap for success rate
+    private static double successRate;
     //------------set-----------//
 
     public static void setRefineLvl(ItemStack stack, int refinelvl) {
@@ -33,8 +42,17 @@ public class RefineUtil {
         int refine = refineValue(refinelvl, maxLvl, startEvasion, endEvasion);
         stack.apply(ModDataComponents.PENOMIOR, NbtComponent.DEFAULT, applied -> applied.apply(compound -> compound.putInt(PenomiorData.EVASION, refine)));
     }
-    public static void setDurability(ItemStack stack, int maxDurability) {
-        stack.apply(ModDataComponents.PENOMIOR, NbtComponent.DEFAULT, applied -> applied.apply(compound -> compound.putInt(PenomiorData.DURABILITY, maxDurability)));
+
+    public static void setDurability(ItemStack stack, int durability) {
+        stack.apply(ModDataComponents.PENOMIOR, NbtComponent.DEFAULT, applied -> applied.apply(compound -> compound.putInt(PenomiorData.DURABILITY, durability)));
+    }
+
+    public static double getSuccessRate() {
+        return successRate;
+    }
+
+    private static void setSuccessRate(double v) {
+        successRate = v;
     }
 
     //------------get-----------//
@@ -49,11 +67,12 @@ public class RefineUtil {
     public static int getEvasion(ItemStack stack) {
         return ItemStackHelper.getNbtCompound(stack, ModDataComponents.PENOMIOR).getInt(PenomiorData.EVASION);
     }
+
     public static int getDurability(ItemStack stack) {
         return ItemStackHelper.getNbtCompound(stack, ModDataComponents.PENOMIOR).getInt(PenomiorData.DURABILITY);
     }
 
-
+    //------------excute-------------//
     public static int refineValue(int currentLvl, int maxLvl, int startAccuracy, int endAccuracy) {
         if (currentLvl < 0 || currentLvl > maxLvl)
             throw new IllegalArgumentException("Input value out of range");
@@ -65,7 +84,38 @@ public class RefineUtil {
         return (int) (startAccuracy + curvedInput * outputRange);
     }
 
-    //------------write-data-----------//
+    public static boolean handleRefine(ItemStack slotOutput, int failStack) {
+        double successRate = calculateSuccessRate(slotOutput, failStack);
+        Random random = new Random();
+        double randomValue = random.nextDouble();
+        // Debug
+        System.out.println("Calculated Success Rate: " + successRate);
+        System.out.println("Random Value: " + randomValue);
+
+        return randomValue < successRate;
+    }
+
+    public static double calculateSuccessRate(ItemStack slotOutput, int failStack) {
+        int currentRefineLvl = getRefineLvl(slotOutput);
+        if (currentRefineLvl >= normalSuccessRates.length)
+            return 0.0; // or handle appropriately for out-of-bounds condition
+
+        double baseSuccessRate = normalSuccessRates[currentRefineLvl] * 0.01;
+        double successRate = baseSuccessRate + (failStack * (baseSuccessRate * 0.1));
+        successRate = Math.min(successRate, SUCCESS_RATE_CAP * 0.01);
+
+        return successRate;
+    }
+    public static void getCalculateSuccessRate(ItemStack slotOutput, int failStack) {
+        setSuccessRate(calculateSuccessRate(slotOutput,failStack));
+    }
+
+
+    public static void successRefine(PlayerEntity player) {
+        ModEntityComponents.STATS.get(player).setFailstack(0);
+    }
+
+    //------------write-data-----------// from craft item
     public static void writeRefineData(ItemStack stack, int refineLvl) {
         String itemID = PenomiorItemData.getItemId(stack);
         PenomiorItemData itemData = PenomiorItemDataSerializer.getConfigCache().get(itemID);
@@ -82,6 +132,13 @@ public class RefineUtil {
                 RefineUtil.setEvasion(stack, refineLvl, maxLvl, startEvasion, endEvasion);
                 RefineUtil.setDurability(stack, maxDurability);
             }
+        }
+    }
+
+    public static void initializeItemData(ItemStack stack, PenomiorItemData itemData) {
+        if (stack.get(ModDataComponents.PENOMIOR) == null) {
+            RefineUtil.setRefineLvl(stack, 0);
+            RefineUtil.setDurability(stack, itemData.maxDurability());
         }
     }
 
