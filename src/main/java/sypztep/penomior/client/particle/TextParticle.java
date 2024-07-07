63e07d4c44411c4667069e7a2e710913b709068b
chai <chainot@hotmail.co.th>
2024-07-08 05:31:13 +0700

    Text Animate

--- src/main/java/sypztep/penomior/client/particle/TextParticle.java
+++ src/main/java/sypztep/penomior/client/particle/TextParticle.java
@@ -6,20 +6,48 @@ import net.minecraft.client.particle.*;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
+import sypztep.penomior.client.particle.util.Easing;
 
 import java.awt.*;
 
 public class TextParticle extends Particle {
     private String text;
-    public TextParticle(ClientWorld world, Vec3d pos, Vec3d velocity) {
-        super(world, pos.x, pos.y, pos.z, velocity.x, velocity.y, velocity.z);
-        velocityMultiplier = 0.99F;
-        gravityStrength = 0.85F;
-        maxAge = 32;
+    private float scale;
+    private final float maxSize;
+
+    public TextParticle(ClientWorld world, double x, double y, double z) {
+        super(world, x, y, z);
+        this.maxAge = 20;
+        this.scale = 0.0F;
+        this.maxSize = - 0.045F;
+    }
+
+    public Particle scale(float scale) {
+        this.scale *= scale;
+        return super.scale(scale);
+    }
+
+    @Override
+    public void tick() {
+        Easing ELASTIC_OUT = new Easing.ElasticOut();
+        if (this.age++ <= 10) {
+            this.scale = MathHelper.lerp(ELASTIC_OUT.ease((float)this.age / 10.0F, 0.0F, 1.0F, 1.0F), 0.0F, this.maxSize);
+            float shakeIntensity = 0.05F;
+            float shakeOffset = (float)Math.sin(this.age * 0.5) * shakeIntensity;
+
+            this.x += shakeOffset;
+            this.z += shakeOffset;
+        } else {
+            this.scale = MathHelper.lerp(Easing.CUBIC_IN.ease(((float)this.age - 10.0F) / 10.0F, 0.0F, 1.0F, 1.0F), this.maxSize, 0.0F);
+        }
+        if (this.age > this.maxAge && this.scale >= 0) {
+            this.markDead();
+        }
     }
 
     public void setText(@NotNull String text) {
@@ -29,7 +57,7 @@ public class TextParticle extends Particle {
     @Override
     public void buildGeometry(VertexConsumer vertexConsumer, Camera camera, float tickDelta) {
         Vec3d cameraPos = camera.getPos();
-        float textSizeScale = -0.045F;
+        float textSizeScale = scale;
         float particleX = (float) (prevPosX + (x - prevPosX) * (double) tickDelta - cameraPos.x);
         float particleY = (float) (prevPosY + (y - prevPosY) * (double) tickDelta - cameraPos.y);
         float particleZ = (float) (prevPosZ + (z - prevPosZ) * (double) tickDelta - cameraPos.z);
