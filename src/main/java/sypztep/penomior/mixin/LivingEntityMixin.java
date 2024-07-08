5eb4507d20c82ea224160ce7c00feba288e9fc96
chai <chainot@hotmail.co.th>
2024-07-08 23:43:39 +0700

    Damage Particle

--- src/main/java/sypztep/penomior/mixin/LivingEntityMixin.java
+++ src/main/java/sypztep/penomior/mixin/LivingEntityMixin.java
@@ -9,6 +9,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 import org.apache.commons.lang3.mutable.MutableInt;
@@ -17,6 +18,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import sypztep.penomior.ModConfig;
@@ -33,20 +35,22 @@ import java.util.List;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
+    @Unique
+    private float previousHealth = 0.0F;
     @Shadow
     public abstract boolean damage(DamageSource source, float amount);
+
     protected LivingEntityMixin(EntityType<?> type, World world) {
         super(type, world);
     }
 
     @Inject(method = "damage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;wakeUp()V", shift = At.Shift.BY, by = 2), cancellable = true)
-    private void applyDamageFirst(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+    private void handleMissing(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         Entity attacker = source.getAttacker();
         MinecraftClient client = MinecraftClient.getInstance();
         ClientWorld world = client.world;
         if (world == null || !world.isClient()) return;
 
-        // this in this method is target
         if (attacker instanceof LivingEntity livingAttacker) {
             StatsComponent targetStats = ModEntityComponents.STATS.getNullable(this); // who take damage
             StatsComponent attackerStats = ModEntityComponents.STATS.getNullable(livingAttacker); // who attack?
@@ -58,13 +62,24 @@ public abstract class LivingEntityMixin extends Entity {
             Identifier identifier = EntityType.getId(livingAttacker.getType());
             for (String id : ModConfig.attackExcludedEntities) {
                 if (identifier != null && identifier.toString().contains(id)) {
-                    return; // Skip particles for match entity that in
+                    return;
                 }
             }
-            // Determine if the attack hits and spawn the particle
-            ParticleUtil.spawnDamageParticle((LivingEntity)(Object) this, amount, attackHits);
-            if (!attackHits)
+            if (!attackHits) {// missing attack
+                ParticleUtil.spawnTextParticle((LivingEntity) (Object) this, Text.translatable("penomior.text.missing"));
                 cir.cancel();
+            }
+        }
+    }
+
+    @Inject(method = "tick()V", at = @At("TAIL"))
+    private void damagenumber(CallbackInfo info) {
+        var entity = (LivingEntity) (Object) this;
+        float oldHealth = previousHealth;
+        float newHealth = entity.getHealth();
+        if (oldHealth != newHealth) {
+            previousHealth = newHealth;
+            ParticleUtil.spawnNumberParticle(entity, Math.abs(newHealth - oldHealth));
         }
     }
 
@@ -73,9 +88,9 @@ public abstract class LivingEntityMixin extends Entity {
         int attackerAccuracy = attackerStats.getAccuracy();
         int targetEvasion = targetStats.getEvasion();
         double hitrate = CombatUtils.calculateHitRate(attackerAccuracy, targetEvasion);
-            System.out.println("Attacker Accuracy: " + attackerAccuracy);
-            System.out.println("Target Evasion: " + targetEvasion);
-            System.out.println("Hit chance: " + hitrate);
+//            System.out.println("Attacker Accuracy: " + attackerAccuracy);
+//            System.out.println("Target Evasion: " + targetEvasion);
+//            System.out.println("Hit chance: " + hitrate);
         Random random = new Random();
         return random.nextDouble() < hitrate * 0.01f;
     }
