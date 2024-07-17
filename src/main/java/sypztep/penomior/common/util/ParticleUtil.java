099b54ffa6ce018d2e566cb00b2ede363a5137d3
chai <chainot@hotmail.co.th>
2024-07-17 21:00:50 +0700

    Overload method to fix text fighting

--- src/main/java/sypztep/penomior/common/util/ParticleUtil.java
+++ src/main/java/sypztep/penomior/common/util/ParticleUtil.java
@@ -11,12 +11,12 @@ import java.awt.*;
 
 public class ParticleUtil {
 
-    private static void spawnParticle(Entity target, String text, Color color, float maxSize) {
+    private static void spawnParticle(Entity target, String text, Color color, float maxSize,float yPos) {
         MinecraftClient client = MinecraftClient.getInstance();
         ClientWorld world = client.world;
         if (world == null || !world.isClient()) return;
 
-        Vec3d particlePos = target.getPos().add(0.0, target.getHeight() + 0.45, 0.0);
+        Vec3d particlePos = target.getPos().add(0.0, target.getHeight() + 0.45 + yPos, 0.0);
         TextParticle particle = new TextParticle(world, particlePos.x, particlePos.y, particlePos.z);
         particle.setText(text);
         particle.setColor(color.getRed(), color.getGreen(), color.getBlue());
@@ -26,17 +26,24 @@ public class ParticleUtil {
     /*
     Spawn particle should be done in client side
      */
+    public static void spawnTextParticle(Entity target, Text text, Color color, float maxSize,float yPos) {
+        if (target.getWorld().isClient())
+            spawnParticle(target, text.getString(), color, maxSize,yPos);
+    }
+    /*
+    Spawn particle should be done in client side
+     */
     public static void spawnTextParticle(Entity target, Text text, Color color, float maxSize) {
         if (target.getWorld().isClient())
-            spawnParticle(target, text.getString(), color, maxSize);
+            spawnParticle(target, text.getString(), color, maxSize,0);
     }
     public static void spawnTextParticle(Entity target, Text text, float maxSize) {
         if (target.getWorld().isClient())
-            spawnParticle(target, text.getString(), new Color(255, 255, 255), maxSize);
+            spawnParticle(target, text.getString(), new Color(255, 255, 255), maxSize,0);
     }
     public static void spawnTextParticle(Entity target, Text text) {
         if (target.getWorld().isClient())
-            spawnParticle(target, text.getString(), new Color(255,255,255), - 0.045F);
+            spawnParticle(target, text.getString(), new Color(255,255,255), - 0.045F,0);
     }
 }
 
