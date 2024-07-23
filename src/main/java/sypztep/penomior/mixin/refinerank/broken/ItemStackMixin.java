3b183ed27fd5a9291b4f21f8785cec22fb227a89
chai <chainot@hotmail.co.th>
2024-07-23 09:34:28 +0700

    Tooltip Modify

--- src/main/java/sypztep/penomior/mixin/refinerank/broken/ItemStackMixin.java
+++ src/main/java/sypztep/penomior/mixin/refinerank/broken/ItemStackMixin.java
@@ -4,6 +4,7 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.component.ComponentHolder;
+import net.minecraft.component.type.AttributeModifiersComponent;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
@@ -13,8 +14,12 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.screen.ScreenTexts;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -46,13 +51,58 @@ public abstract class ItemStackMixin implements ComponentHolder {
     }
 
     @ModifyVariable(method = "appendAttributeModifierTooltip", at = @At(value = "STORE"))
-    private double resetAttributeValue(double value, @Local(ordinal = 0, argsOnly = true) EntityAttributeModifier modifier, @Local(ordinal = 0, argsOnly = true) PlayerEntity player) {
+    private double resetAttributeValue(double value, @Local(ordinal = 0, argsOnly = true) RegistryEntry<EntityAttribute> attribute, @Local(ordinal = 0, argsOnly = true) EntityAttributeModifier modifier, @Local(ordinal = 0, argsOnly = true) PlayerEntity player) {
         if (player != null) {
             if (RefineUtil.isBroken(stack) && modifier.idMatches(Item.BASE_ATTACK_DAMAGE_MODIFIER_ID))
                 return player.getAttributeBaseValue(EntityAttributes.GENERIC_ATTACK_DAMAGE);
             else if (stack.isIn(ItemTags.ARMOR_ENCHANTABLE) && RefineUtil.isBroken(stack)) return 0;
         }
         return value;
+    }
+
+    @Inject(method = "appendAttributeModifierTooltip", at = @At(value = "INVOKE", target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V", ordinal = 0), cancellable = true)
+    private void modifyExtraDamage(Consumer<Text> textConsumer, @Nullable PlayerEntity player, RegistryEntry<EntityAttribute> attribute, EntityAttributeModifier modifier, CallbackInfo ci) {
+        if (player == null) return;
+
+        double d = computeDamageValue(modifier, player);
+        int extraDamage = RefineUtil.getExtraDamage(stack);
+
+        if (!RefineUtil.isBroken(stack) && modifier.idMatches(Item.BASE_ATTACK_DAMAGE_MODIFIER_ID) && RefineUtil.getRefineLvl(stack) > 0) {
+            textConsumer.accept(createText("penomior.attribute.modifier.equals.0", d, extraDamage));
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "appendAttributeModifierTooltip", at = @At(value = "INVOKE", target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V", ordinal = 1), cancellable = true)
+    private void modifyExtraProtect(Consumer<Text> textConsumer, @Nullable PlayerEntity player, RegistryEntry<EntityAttribute> attribute, EntityAttributeModifier modifier, CallbackInfo ci) {
+        if (player == null) return;
+
+        double d = computeDamageValue(modifier, player);
+        int extraProtect = RefineUtil.getExtraProtect(stack);
+
+        if (stack.isIn(ItemTags.ARMOR_ENCHANTABLE) && !RefineUtil.isBroken(stack) && attribute.matches(EntityAttributes.GENERIC_ARMOR) && RefineUtil.getRefineLvl(stack) > 0) {
+            textConsumer.accept(createText("penomior.attribute.modifier.plus.1", d, extraProtect));
+            ci.cancel();
+        }
+    }
+
+    @Unique
+    private double computeDamageValue(EntityAttributeModifier modifier, PlayerEntity player) {
+        double baseValue = player.getAttributeBaseValue(EntityAttributes.GENERIC_ATTACK_DAMAGE) + modifier.value();
+        return modifier.operation() == EntityAttributeModifier.Operation.ADD_MULTIPLIED_BASE ||
+                modifier.operation() == EntityAttributeModifier.Operation.ADD_MULTIPLIED_TOTAL
+                ? baseValue * 100.0
+                : baseValue;
+    }
+
+    @Unique
+    private Text createText(String key, double value, int extra) {
+        return ScreenTexts.space()
+                .append(Text.translatable(
+                        key,
+                        AttributeModifiersComponent.DECIMAL_FORMAT.format(value),
+                        "+" + AttributeModifiersComponent.DECIMAL_FORMAT.format(extra)
+                ).formatted(Formatting.DARK_GREEN));
     }
 
     @Inject(method = "damage(ILnet/minecraft/server/world/ServerWorld;Lnet/minecraft/server/network/ServerPlayerEntity;Ljava/util/function/Consumer;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;"), cancellable = true)
