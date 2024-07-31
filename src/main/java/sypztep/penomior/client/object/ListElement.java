31a134f13286da3434e2089d443256353a71cd12
chai <chainot@hotmail.co.th>
2024-07-31 22:56:51 +0700

    icon and constuceture

---
+++ src/main/java/sypztep/penomior/client/object/ListElement.java
@@ -0,0 +1,11 @@
+package sypztep.penomior.client.object;
+
+import net.minecraft.util.Identifier;
+
+public record ListElement(String text, Identifier icon) {
+    public ListElement(String text) { // constuct
+        this(text,null);
+    }
+
+}
+
