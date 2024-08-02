ca8153cc4cc57fe3764f7d06fe1666f30260ef02
chai <chainot@hotmail.co.th>
2024-08-02 14:01:42 +0700

    Bumb

--- src/main/java/sypztep/penomior/client/object/ListElement.java
+++ src/main/java/sypztep/penomior/client/object/ListElement.java
@@ -3,9 +3,7 @@ package sypztep.penomior.client.object;
 import net.minecraft.util.Identifier;
 
 public record ListElement(String text, Identifier icon) {
-    public ListElement(String text) { // constuct
+    public ListElement(String text) {
         this(text,null);
     }
-
 }
-
