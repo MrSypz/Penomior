c4893ae6b17e66df36ba52d20f4bf53f142fea3b
chai <chainot@hotmail.co.th>
2024-08-16 22:06:17 +0700

    Remove indev

--- src/main/java/sypztep/penomior/mixin/vanilla/critattribute/PlayerEntityMixin.java
+++ src/main/java/sypztep/penomior/mixin/vanilla/critattribute/PlayerEntityMixin.java
@@ -1,16 +1,18 @@
 package sypztep.penomior.mixin.vanilla.critattribute;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import sypztep.penomior.ModConfig;
+import sypztep.penomior.client.payload.AddTextParticlesPayload;
 import sypztep.penomior.common.init.ModEntityAttributes;
 
 
@@ -23,21 +25,16 @@ public abstract class PlayerEntityMixin extends LivingEntity {
 
     @ModifyExpressionValue(method = "attack", at = @At(value = "CONSTANT", args = "floatValue=1.5"))
     private float applyCritDmg(float original) {
-        if (ModConfig.inDev) {
+        PlayerLookup.tracking((ServerWorld) this.getWorld(), this.getChunkPos()).forEach(foundPlayer -> AddTextParticlesPayload.send(foundPlayer, this.getId(), AddTextParticlesPayload.TextParticle.CRITICAL)); // Attacker
             return (float) this.getAttributeValue(ModEntityAttributes.GENERIC_CRIT_DAMAGE);
-        }
-//        PlayerLookup.tracking((ServerWorld) this.getWorld(), this.getChunkPos()).forEach(foundPlayer -> AddTextParticlesPayload.send(foundPlayer, this.getId(), AddTextParticlesPayload.TextParticle.CRITICAL)); // Attacker
-        return original;
     }
+
     @ModifyVariable(method = "attack", at = @At("STORE"), ordinal = 2)
     private boolean doCrit(boolean original, Entity target) {
-        if (ModConfig.inDev) {
-            double critChance = this.getAttributeValue(ModEntityAttributes.GENERIC_CRIT_CHANCE);
-            if (!(critChance > 0))
-                return false;
-            else
-                return target instanceof LivingEntity && this.random.nextFloat() < critChance && !this.hasStatusEffect(StatusEffects.BLINDNESS);
-        }
-        return original;
+        double critChance = this.getAttributeValue(ModEntityAttributes.GENERIC_CRIT_CHANCE);
+        if (!(critChance > 0))
+            return false;
+        else
+            return target instanceof LivingEntity && this.random.nextFloat() < critChance && !this.hasStatusEffect(StatusEffects.BLINDNESS);
     }
 }
