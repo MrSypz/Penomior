e9f05179addbf8377e89f7634509fbfe7d53d5ee
chai <chainot@hotmail.co.th>
2024-08-18 21:10:52 +0700

    projectile clean up

---
+++ src/main/java/sypztep/penomior/mixin/vanilla/critattribute/LivingEntityMixin.java
@@ -0,0 +1,34 @@
+package sypztep.penomior.mixin.vanilla.critattribute;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import sypztep.penomior.common.init.ModEntityAttributes;
+import sypztep.penomior.common.util.CombatUtils;
+import sypztep.penomior.common.util.interfaces.MissingAccessor;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin extends Entity {
+    public LivingEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+    @ModifyVariable(method = "applyDamage", at = @At("HEAD"), ordinal = 0, argsOnly = true)
+    private float applyDamageFirst(float amount, DamageSource source) {
+        if (!this.getWorld().isClient()) {
+
+            Entity projectileSource = source.getSource();
+            if (projectileSource instanceof PersistentProjectileEntity projectile && CombatUtils.doCrit((LivingEntity) source.getAttacker()) && source.getAttacker() instanceof MissingAccessor accessor && !accessor.penomior$isMissing()) {
+                projectile.setCritical(true);
+                CombatUtils.applyParticle(source.getSource());
+                return (float) (amount * ((LivingEntity) source.getAttacker()).getAttributeValue(ModEntityAttributes.GENERIC_CRIT_DAMAGE));
+            }
+        }
+        return amount;
+    }
+}
