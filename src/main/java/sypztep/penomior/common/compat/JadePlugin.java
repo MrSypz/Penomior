1fb7b703fa2589148e7d201985094ce47b320fff
chai <chainot@hotmail.co.th>
2024-09-18 09:46:58 +0700

    bumb

--- src/main/java/sypztep/penomior/common/compat/JadePlugin.java
+++ src/main/java/sypztep/penomior/common/compat/JadePlugin.java
@@ -9,12 +9,10 @@ import sypztep.penomior.common.compat.provider.EntityComponentProvider;
 public class JadePlugin implements IWailaPlugin {
     @Override
     public void register(IWailaCommonRegistration registration) {
-        //TODO register data providers
     }
 
     @Override
     public void registerClient(IWailaClientRegistration registration) {
-        //TODO register component providers, icon providers, callbacks, and config options here
         registration.registerEntityComponent(EntityComponentProvider.INSTANCE, LivingEntity.class);
 
     }
