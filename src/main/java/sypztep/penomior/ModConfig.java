f75865f232b014a0d0dcf13f4bbd896f38720232
chai <chainot@hotmail.co.th>
2024-10-27 17:56:54 +0700

    More Easy Stats Map reduce max Level to 99

--- src/main/java/sypztep/penomior/ModConfig.java
+++ src/main/java/sypztep/penomior/ModConfig.java
@@ -70,29 +70,35 @@ public class ModConfig implements ConfigData {
     @ConfigEntry.Category("statconfig_gameplay")
     public static int[] statPointLadder = new int[] {
             // Levels 1-10
-            1, 1, 2, 2, 3, 3, 4, 4, 5, 5,
+            2,
             // Levels 11-20
-            6, 6, 7, 7, 8, 8, 9, 9, 10, 10,
+            3,
             // Levels 21-30
-            11, 11, 12, 12, 13, 13, 14, 14, 15, 15,
+            4,
             // Levels 31-40
-            16, 16, 17, 17, 18, 18, 19, 19, 20, 20,
+            5,
             // Levels 41-50
-            21, 21, 22, 22, 23, 23, 24, 24, 25, 25,
+            6,
             // Levels 51-60
-            26, 26, 27, 27, 28, 28, 29, 29, 30, 30,
+            7,
             // Levels 61-70
-            31, 31, 32, 32, 33, 33, 34, 34, 35, 35,
+            8,
             // Levels 71-80
-            36, 36, 37, 37, 38, 38, 39, 39, 40, 40,
+            9,
             // Levels 81-90
-            41, 41, 42, 42, 43, 43, 44, 44, 45, 45,
+            10,
             // Levels 91-100
-            46, 46, 47, 47, 48, 48, 49, 49, 50, 50
+            11,
+            // Levels 101-110
+            12,
+            // Levels 111-120
+            13,
+            // Levels 121-130
+            14
     };
     @ConfigEntry.Category("statconfig_gameplay")
     @ConfigEntry.BoundedDiscrete(min = 1,max = Integer.MAX_VALUE)
-    public static int maxLevel = 100;
+    public static int maxLevel = 99;
     @ConfigEntry.Category("statconfig_gameplay")
     @ConfigEntry.BoundedDiscrete(min = 0,max = Integer.MAX_VALUE)
     public static double exponentExp = 1.5;
