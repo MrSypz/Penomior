842486a15446b7875a6b16fa2829ba13b9f67a74
chai <chainot@hotmail.co.th>
2024-10-27 21:21:58 +0700

    New Calculate System

--- src/main/java/sypztep/penomior/common/util/CombatUtils.java
+++ src/main/java/sypztep/penomior/common/util/CombatUtils.java
@@ -4,38 +4,87 @@ import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.MathHelper;
 import sypztep.penomior.client.payload.AddTextParticlesPayload;
 import sypztep.penomior.common.component.StatsComponent;
+import sypztep.penomior.common.component.UniqueStatsComponent;
 import sypztep.penomior.common.init.ModEntityAttributes;
+import sypztep.penomior.common.stats.StatTypes;
 import sypztep.penomior.common.tag.ModDamageTags;
 
 import java.util.Random;
 
 public final class CombatUtils {
     // Constants
-    private static final int BASE_HIT_RATE = 80;
-    private static final int MAX_HIT_RATE = 95;
+    private static final int BASE_HIT = 175;
+    private static final int BASE_FLEE = 100;
     private static final int MIN_HIT_RATE = 5;
+    private static final int MAX_FLEE_RATE = 95;
 
-    public static double calculateHitRate(int totalAccuracy, int totalEvasion) {
-        // Base formula: HitRate = 80 + (Attacker's Hit - Target's Flee)
-        int hitRate = BASE_HIT_RATE + (totalAccuracy - totalEvasion);
-        return MathHelper.clamp(hitRate,MIN_HIT_RATE,MAX_HIT_RATE);
+    public static double calculateHitRate(double hitRate, double fleeRate) {
+        return Math.max(hitRate - fleeRate, MIN_HIT_RATE);
     }
 
-    public static boolean isAttackHits(StatsComponent attackerStats, StatsComponent targetStats) {
+    public static double hitRate(UniqueStatsComponent attackerStats, UniqueStatsComponent targetStats, int totalAttackerAccuracy, int totalTargetEvasion) {
+        int hitPoint = hit(attackerStats, totalAttackerAccuracy);
+        double hitRate = 0;
+
+        if (targetStats.getObj() instanceof PlayerEntity) {
+            hitRate = hitPoint - totalTargetEvasion;
+        } else if (targetStats.getObj() instanceof MobEntity) {
+            int agi = targetStats.getLivingStats().getStat(StatTypes.AGILITY).getValue();
+            int lvl = targetStats.getLevel();
+            int hitRequire = 200 + lvl + agi;
+            hitRate = 100 + hitPoint - hitRequire;
+        }
+
+        return MathHelper.clamp(hitRate, MIN_HIT_RATE, 100);
+    }
+
+    public static double fleeRate(UniqueStatsComponent attackerStats, UniqueStatsComponent targetStats, int totalTargetEvasion, int totalAttackerAccuracy, int attackers) {
+        int fleePoint = flee(targetStats, totalTargetEvasion, attackers);
+        double fleeRate = 0;
+
+        if (targetStats.getObj() instanceof PlayerEntity) {
+            int hitPoint = hit(attackerStats, totalAttackerAccuracy);
+            fleeRate = 100 - (hitPoint - fleePoint);
+        } else if (targetStats.getObj() instanceof MobEntity) {
+            int dex = targetStats.getLivingStats().getStat(StatTypes.DEXTERITY).getValue();
+            int lvl = targetStats.getLevel();
+            int fleeRequire = 170 + lvl + dex;
+            fleeRate = 95 + fleePoint - fleeRequire;
+        }
+
+        return MathHelper.clamp(fleeRate, 0, MAX_FLEE_RATE);
+    }
+    private static boolean isAttackHits(StatsComponent attackerStats, StatsComponent targetStats, UniqueStatsComponent attacker, UniqueStatsComponent target) {
         int attackerAccuracy = attackerStats.getAccuracy();
         int targetEvasion = targetStats.getEvasion();
-        double hitrate = CombatUtils.calculateHitRate(attackerAccuracy, targetEvasion);
+        double hitRate = hitRate(attacker, target, attackerAccuracy, targetEvasion);
+        double fleeRate = fleeRate(attacker, target, targetEvasion, attackerAccuracy, 1);
+
+        double effectiveHitRate = calculateHitRate(hitRate, fleeRate);
+
         Random random = new Random();
-        return random.nextDouble() < hitrate * 0.01f;
+        double chance = random.nextDouble() * 100;
+        return chance < effectiveHitRate;
     }
 
-    public static boolean isMissingHits(StatsComponent attackerStats, StatsComponent targetStats) {
-        return !isAttackHits(attackerStats, targetStats);
+    public static boolean isMissingHits(StatsComponent attackerStats, StatsComponent targetStats, UniqueStatsComponent attackerComponent, UniqueStatsComponent targetComponent) {
+        return !isAttackHits(attackerStats, targetStats, attackerComponent, targetComponent);
+    }
+
+    private static int hit(UniqueStatsComponent levelComponent, int totalAccuracy) {
+        int baseLvl = levelComponent.getLevel();
+        return BASE_HIT + baseLvl + totalAccuracy;
+    }
+
+    private static int flee(UniqueStatsComponent levelComponent, int totalEvasion, int attackers) {
+        int baseLvl = levelComponent.getLevel();
+        return (int) (BASE_FLEE + (baseLvl + totalEvasion) * (1 - (attackers - 2) * 0.1f));
     }
 
     public static float damageModifier(LivingEntity target, float amount, DamageSource source) {
@@ -77,13 +126,15 @@ public final class CombatUtils {
         float finalDamage = amplifiedDamage - projectileResistance;
         return Math.max(finalDamage, 0);
     }
+
     private static float calculatePlayerVersPlayer(LivingEntity target, float value) {
         float pvpAttackMultiplier = (float) target.getAttributeValue(ModEntityAttributes.GENERIC_PLAYER_VERS_PLAYER_DAMAGE);
-        return Math.max(value + value * pvpAttackMultiplier,0);
+        return Math.max(value + value * pvpAttackMultiplier, 0);
     }
+
     private static float calculatePlayerVersEntity(LivingEntity target, float value) {
         float pveAttackMultiplier = (float) target.getAttributeValue(ModEntityAttributes.GENERIC_PLAYER_VERS_ENTITY_DAMAGE);
-        return Math.max(value + value * pveAttackMultiplier,0);
+        return Math.max(value + value * pveAttackMultiplier, 0);
     }
 
     private static float calculateMeleeDamage(LivingEntity target, float value) {
