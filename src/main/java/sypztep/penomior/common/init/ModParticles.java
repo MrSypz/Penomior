2aaed2b788f667c4799c2b675912f997cb773770
chai <chainot@hotmail.co.th>
2024-12-24 07:37:07 +0700

    Port back to 1.20 Particle type

--- src/main/java/sypztep/penomior/common/init/ModParticles.java
+++ src/main/java/sypztep/penomior/common/init/ModParticles.java
@@ -1,14 +1,14 @@
 package sypztep.penomior.common.init;
 
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
+import net.minecraft.particle.DefaultParticleType;
 import net.minecraft.particle.ParticleType;
-import net.minecraft.particle.SimpleParticleType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import sypztep.penomior.Penomior;
 
 public class ModParticles {
-    public static final SimpleParticleType TEXT_PARTICLE = FabricParticleTypes.simple();
+    public static final DefaultParticleType TEXT_PARTICLE = FabricParticleTypes.simple();
 
     public static void init() {
         registerParticle(TEXT_PARTICLE,"text_particle");
